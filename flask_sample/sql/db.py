8fd61292e37cb9de4a395b7ad050d45937e6ad5a
THARUN BHARADWAJ <tbb@njit.edu>
2022-12-12 02:31:01 -0500

    flask_flash

--- flask_sample/sql/db.py
+++ flask_sample/sql/db.py
@@ -6,8 +6,7 @@ class CRUD(Enum):
     CREATE = 1,
     READ = 2,
     UPDATE = 3,
-    DELETE = 4, 
-    ALTER = 5
+    DELETE = 4
 
 
 class DBResponse:
@@ -15,12 +14,8 @@ class DBResponse:
         self.status = status
         if row is not None:
             self.row = row
-        else:
-            self.row = None # return none
         if rows is not None:
             self.rows = rows
-        else:
-            self.rows = [] # return empty list
     def __str__(self):
         return json.dumps(self.__dict__)
 
@@ -28,19 +23,15 @@ class DB:
     db = None
     def __runQuery(op, isMany, queryString, args = None):
         response = None
-       
         try:
             db = DB.getDB()
             cursor = db.cursor(dictionary=True)
             status = False
-            if not isMany or op == CRUD.READ:
+            
+            if not isMany or CRUD.READ:
                 if args is not None and len(args) > 0:
-                    # convert dict for named placeholder mapping
-                    if type(args[0]) is dict:
-                        args = {k: v for d in args for k, v in d.items()}
                     status = cursor.execute(queryString, args)
                 else:
-                    
                     status = cursor.execute(queryString)
             else:
                 if args is not None and len(args) > 0:
@@ -58,17 +49,12 @@ class DB:
                     status = True if status is None else False
                     response = DBResponse(status, None, result)
             else:
+                db.commit()
                 status = True if status is None else False
                 response = DBResponse(status)
-            try:
+            if db.is_connected():
                 cursor.close()
-            except Exception as ce:
-                print("cursor close error", ce)
-        
         except Error as e:
-            if e.errno == -1:
-                print("closing due to error")
-                DB.close()
             # converting to a plain exception so other modules don't need to import mysql.connector.Error
             # this will let you more easily swap out DB connectors without needing to refactor your code, just this class
             raise Exception(e)
@@ -87,12 +73,9 @@ class DB:
         if "CREATE TABLE" in queryString.upper():
             return DB.__runQuery(CRUD.CREATE, False, queryString)
         elif queryString.upper().startswith("ALTER"):
-            return DB.__runQuery(CRUD.ALTER, False, queryString)
-        elif queryString.upper().startswith("INSERT"):
-            return DB.insertOne(queryString, )
+            return DB.__runQuery(CRUD.UPDATE, False, queryString)
         else:
-            return DB.__runQuery(CRUD.ALTER, False, queryString)
-            #raise Exception("Please use one of the abstracted methods for this query")
+            raise Exception("Please use one of the abstracted methods for this query")
 
     @staticmethod
     def insertMany(queryString, data):
@@ -114,15 +97,13 @@ class DB:
     
     @staticmethod
     def close():
-        try:
+        if DB.db and DB.db.is_connected:
             DB.db.close()
-        except:
-            pass
-        DB.db = None
+            DB.db = None
 
     @staticmethod
     def getDB():
-        if DB.db is None or DB.db.is_connected() == False:
+        if DB.db is None:
             import mysql.connector
             import os
             import re
@@ -135,9 +116,7 @@ class DB:
                 if len(data) >= 5:
                     try:
                         user,password,host,port,database = data
-                        DB.db = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port,
-                        connection_timeout=10)
-                        DB.db.autocommit = True
+                        DB.db = mysql.connector.connect(host=host, user=user, password=password, database=database, port=port)
                     except Error as e:
                         print("Error while connecting to MySQL", e)
                 else:
