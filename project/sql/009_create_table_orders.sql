6d134bfc3dba046ce5585bd7a26e4e24c9ffcb49
THARUN BHARADWAJ <tbb@njit.edu>
2022-12-22 21:15:40 -0500

    adding files

--- project/sql/009_create_table_orders.sql
+++ project/sql/009_create_table_orders.sql
@@ -1,9 +1,13 @@
 CREATE TABLE
     IF NOT EXISTS IS601_S_Orders(
         id int AUTO_INCREMENT PRIMARY KEY,
-        total_spent int,
-        number_of_items int,
         user_id int,
+        total_price float,
+        address VARCHAR(200),
+        payment_method VARCHAR(30),
+        money_received float,
+        first_name varchar(30),
+        last_name varchar(15),
         created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
         FOREIGN KEY (user_id) REFERENCES IS601_Users(id)
--- project/sql/010_create_table_orderitems.sql
+++ project/sql/010_create_table_orderitems.sql
@@ -2,18 +2,16 @@ CREATE TABLE
     IF NOT EXISTS IS601_S_OrderItems(
         id int AUTO_INCREMENT PRIMARY KEY,
         quantity int DEFAULT 0,
-        cost int DEFAULT 99999,
+        unit_price int DEFAULT 99999,
         order_id int,
-        item_id int,
-        user_id int,
+        product_id int,
         created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
         check (quantity >= 0),
         -- don't allow negative stock; I don't allow backorders
-        check (cost >= 0),
+        check (unit_price >= 0),
         -- don't allow negative costs
-        FOREIGN KEY (user_id) REFERENCES IS601_Users(id),
-        FOREIGN KEY (item_id) REFERENCES IS601_S_Items(id),
+        FOREIGN KEY (product_id) REFERENCES IS601_S_Products(id),
         FOREIGN KEY (order_id) REFERENCES IS601_S_Orders(id),
-        UNIQUE KEY (order_id, item_id, user_id)
+        UNIQUE KEY (order_id, product_id)
     )
