6dfca134e2d4d04148c2dcf718032070f27954de
THARUN BHARADWAJ <tbb@njit.edu>
2022-12-22 21:56:52 -0500

    pushing empty submission file

--- project/main.py
+++ project/main.py
@@ -35,8 +35,8 @@ def create_app(config_filename=''):
     with app.app_context():
         #from views.hello import hello
         #app.register_blueprint(hello)
-        from views.sample import sample
-        app.register_blueprint(sample)
+        #from views.sample import sample
+        #app.register_blueprint(sample)
         from auth.auth import auth
         app.register_blueprint(auth)
         from roles.roles import roles
